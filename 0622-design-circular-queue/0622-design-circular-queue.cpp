00bd721445820ae5c13b01c056a8823f781ab3f1
Priyanshu Raj Sharma <151444290+Priyanshu001234@users.noreply.github.com>
2026-07-15 15:30:35 +0530

    Time: 3 ms (62.56%), Space: 23.4 MB (82.54%) - LeetHub

---
+++ 0622-design-circular-queue/0622-design-circular-queue.cpp
@@ -0,0 +1,61 @@
+class MyCircularQueue {
+public:
+    vector<int> data;
+    int front;
+    int rear;
+    int size;
+    int count;
+
+    MyCircularQueue(int k) {
+        data.resize(k);      // Initialize vector
+        front = 0;
+        rear = -1;
+        count = 0;
+        size = k;
+    }
+
+    bool enQueue(int value) {
+        if (isFull()) {
+            return false;
+        }
+
+        rear = (rear + 1) % size;
+        data[rear] = value;
+        count++;
+        return true;
+    }
+
+    bool deQueue() {
+        if (isEmpty()) {
+            return false;
+        }
+
+        front = (front + 1) % size;
+        count--;
+        return true;
+    }
+
+    int Front() {
+        if (isEmpty()) {
+            return -1;
+        }
+
+        return data[front];
+    }
+
+    int Rear() {
+        if (isEmpty()) {
+            return -1;
+        }
+
+        return data[rear];
+    }
+
+    bool isEmpty() {
+        return count == 0;
+    }
+
+    bool isFull() {
+        return count == size;
+    }
+};
